fae2f89b8c4a12b152588ab5424d22e71612999a
fore4years <2107450225@qq.com>
2025-12-31 16:57:26 +0800

    修改页脚和“关于”

--- src/config.ts
+++ src/config.ts
@@ -414,7 +414,7 @@ export const commentConfig: CommentConfig = {
 
 export const announcementConfig: AnnouncementConfig = {
 	title: "Announcement", // 公告标题
-	content: "Welcome to my blog! This is a sample announcement.", // 公告内容
+	content: "记录着我的琐碎日常", // 公告内容
 	closable: true, // 允许用户关闭公告
 	link: {
 		enable: true, // 启用链接
@@ -436,7 +436,14 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 
 export const footerConfig: FooterConfig = {
 	enable: true, // 是否启用Footer HTML注入功能
-	customHtml: "蜀ICP备2025177764号 | <img src=\"https://beian.mps.gov.cn/web/assets/logorr01.6189a29f.png\" style=\"width: 16px;height: 16px;margin: 04px;vertical-align:middle;\" alt=\"公安备案图标\"> 川公网安备51011402000992号", // HTML格式的自定义页脚信息，例如备案号等，默认留空
+	customHtml: `
+		<span style="display: inline-flex; align-items: center; gap: 8px;">
+			蜀ICP备2025177764号
+			|
+			<img src="https://beian.mps.gov.cn/web/assets/logorr01.6189a29f.png" style="width: 16px; height: 16px; vertical-align: middle;" alt="公安备案图标">
+			川公网安备51011402000992号
+		</span>
+	`, // HTML格式的自定义页脚信息，例如备案号等，默认留空
 	// 也可以直接编辑 FooterConfig.html 文件来添加备案号等自定义内容
 	// 注意：若 customHtml 不为空，则使用 customHtml 中的内容；若 customHtml 留空，则使用 FooterConfig.html 文件中的内容
 	// FooterConfig.html 可能会在未来的某个版本弃用
